575dd6557d3ffd19a2722792ff7a3a1434fe14eb
Connor FitzGerald <connorreecefitzgerald@gmail.com>
2019-01-24 10:39:02 -0500

    fixed the cooking exercise

--- 1-23-2019/functions-problems/script.js
+++ 1-23-2019/functions-problems/script.js
@@ -141,5 +141,7 @@ function grill (currentObject) {
     cookedFood.push(currentObject);
 };
 // Your task is to iterate over the array of foods and invoke the function for each item so that the cookedFood array contains all of the items after they are cooked.
-grill(foods)
+for(let i = 0; i<foods.length; i++){
+    grill(foods[i])
+}
 console.log(cookedFood)
