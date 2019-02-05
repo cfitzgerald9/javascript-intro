7d3b853f7db3653ecffc8e6846c0ab20f817214f
Connor FitzGerald <connorreecefitzgerald@gmail.com>
2019-02-05 13:48:06 -0500

    fixing git pt. 10000000

---
+++ 1-23-2019/blog-site-dom/blogsiterevisit/script.js
@@ -0,0 +1,16 @@
+// Use JavaScript to obtain a reference to the first section with the class of article_header and change its text with the textContent property to "Welcome to the {insert your name here} blog"
+const wooHoo = document.querySelector(".article__header")
+console.log(wooHoo)
+// Use JavaScript to obtain a reference to all article__header elements and change their classList property value to article__header important.
+const allHead = document.querySelectorAll(".article__header")
+console.log(allHead)
+for(let i = 0; i< allHead.length; i++){
+    allHead[i].classList = "article__header important"
+}
+
+// Obtain a reference the element with a class of dashed and remove it.
+const baleetThis = document.querySelector(".dashed")
+baleetThis.classList.remove("dashed")
+// Obtain a reference the element with a class of article_footer and add the class of goldenrod it.
+const addThis = document.querySelector(".article__footer")
+addThis.classList.add("goldenrod")
